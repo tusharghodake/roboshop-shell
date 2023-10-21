42b0618a5f0239d9af5b04c6ed74a7dc9ce1e764
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-21 20:50:51 +0530

    common functions added

---
+++ common.sh
@@ -0,0 +1,17 @@
+script_location=$(pwd)
+LOG=/tmp/roboshop.log
+
+status_check(){
+  if [ $? -eq 0 ]
+  then
+    echo -e "\e[32m SUCCESS\e[0m"
+  else
+    echo -e "\e[31m FAILURE\e[0m"
+    echo "Refer log file for more information, LOG- ${LOG}"
+    exit
+  fi
+}
+
+print_head(){
+  echo -e "\e[35m $1 \e[0m"
+}
