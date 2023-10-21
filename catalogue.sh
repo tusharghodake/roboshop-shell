9798f02bcd7495be5957505b945c5bdc185e4cf3
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-21 20:51:22 +0530

    print_head() used

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,6 @@
+source common.sh
 
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
-
-status_check(){
-  if [ $? -eq 0 ]
-  then
-    echo -e "\e[32m SUCCESS\e[0m"
-  else
-    echo -e "\e[31m FAILURE\e[0m"
-    echo "Refer log file for more information, LOG- ${LOG}"
-    exit
-  fi
-}
-
-echo -e "\e[35m Configuring NodeJS Repo\e[0m"
+print_head "Configuring NodeJS Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 status_check
 
