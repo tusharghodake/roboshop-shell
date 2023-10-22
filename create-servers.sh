eaefa1d6d1e8cd5b7349978b673df0051c37f686
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-22 15:35:48 +0530

    code updated

--- create-servers.sh
+++ create-servers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##### Change these values ###
-ZONE_ID="Z0366464237Z7LZLZPKFA"
-DOMAIN="devopsb70.online"
+ZONE_ID="Z04184682PQFE366OMS8O"
+DOMAIN="tushardevops.online"
 SG_NAME="allow-all"
 env=dev
 #############################
