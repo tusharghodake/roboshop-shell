6f5a62e74c5a06b860080af1202e6e2c66d27d70
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-22 10:41:13 +0530

    catalogue.sh updated

--- catalogue.sh
+++ catalogue.sh
@@ -4,60 +4,60 @@ print_head "Configuring NodeJS Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 status_check
 
-echo -e "\e[35m Install NodeJS\e[0m"
+print_head " Install NodeJS"
 dnf install nodejs -y &>>${LOG}
 status_check
 
-echo -e "\e[35m Add Application User\e[0m"
+print_head "Add Application User"
 useradd roboshop &>>${LOG}
 status_check
 
-echo -e "\e[35m Create App Directory\e[0m"
+print_head "Create App Directory"
 mkdir -p /app &>>${LOG}
 status_check
 
-echo -e "\e[35m Downloading App Content\e[0m"
+print_head "Downloading App Content"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
 status_check
 
-echo -e "\e[35m Cleanup Old Content\e[0m"
+print_head "Cleanup Old Content"
 rm -rf /app/* &>>${LOG}
 status_check
 
-echo -e "\e[35m Extracting App Content\e[0m"
+print_head "Extracting App Content"
 cd /app 2&>>${LOG}
 unzip /tmp/catalogue.zip  &>>${LOG}
 status_check
 
-echo -e "\e[35m Installing NodeJS Dependencies\e[0m"
+print_head "Installing NodeJS Dependencies"
 cd /app 2>>${LOG}
 npm install &>>${LOG}
 status_check
 
-echo -e "\e[35m Configuring Catalogue Service File\e[0m"
+print_head "Configuring Catalogue Service File"
 cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${LOG}
 status_check
 
-echo -e "\e[35m Reload SystemD\e[0m"
+print_head "Reload SystemD"
 systemctl daemon-reload &>>${LOG}
 status_check
 
-echo -e "\e[35m Enable catalogue Service\e[0m"
+print_head "Enable catalogue Service"
 systemctl enable catalogue &>>${LOG}
 status_check
 
-echo -e "\e[35m Start catalogue Service\e[0m"
+print_head "Start catalogue Service"
 systemctl start catalogue &>>${LOG}
 status_check
 
-echo -e "\e[35m Configuring mongo repo\e[0m"
+print_head "Configuring mongo repo"
 cp ${script_location}/files/mongodb.repo  /etc/yum.repos.d/mongodb.repo &>>${LOG}
 status_check
 
-echo -e "\e[35m Install mongo client\e[0m"
+print_head "Install mongo client"
 dnf install mongodb-org-shell -y &>>${LOG}
 status_check
 
-echo -e "\e[35m Schema Loading\e[0m"
+print_head "Schema Loading"
 mongo --host localhost </app/schema/catalogue.js &>>${LOG}
 status_check
