5513832623f1043602bf30c4e4622314860c7a7a
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-22 17:57:31 +0530

    code updated

--- common.sh
+++ common.sh
@@ -72,7 +72,7 @@ LOAD_SCHEMA() {
       status_check
 
       print_head "Load Schema"
-      mongo --host mongodb-dev.devopsb70.online </app/schema/${component}.js &>>${LOG}
+      mongo --host mongodb-dev.tushardevops.online </app/schema/${component}.js &>>${LOG}
       status_check
     fi
 
