56b74a4582721a1caff29f151ed9053932025c78
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-22 19:07:30 +0530

    code updated

--- common.sh
+++ common.sh
@@ -83,7 +83,7 @@ LOAD_SCHEMA() {
       status_check
 
       print_head "Load Schema"
-      mysql -h mysql-dev.devopsb70.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
+      mysql -h mysql-dev.tushardevops.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
       status_check
     fi
 
